cccee196724a8fd1f8ea0ada5ed149362bc6ef71
Lucas-Winicius <lucasclucas7@gmail.com>
2023-05-28 22:59:16 -0300

    Create user

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const routes = require("./src/router");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SIGNATURE));
 app.use(routes);
 
 app.listen(process.env.PORT, () => {
--- src/controller/user.js
+++ src/controller/user.js
@@ -1,5 +1,46 @@
-function create(req, res) {
-  res.send(`Creating ${req.body.name}!`);
+const jwt = require("jsonwebtoken");
+const prisma = require("../lib/prisma");
+
+async function create(req, res) {
+  const { name, password } = req.body;
+
+  if (!name || !password) {
+    return res.status(400).json({
+      status: 400,
+      message:
+        "Please fill in all fields correctly, including name and password.",
+    });
+  }
+
+  if (password.length < 8 || password.length > 255) {
+    return res.status(400).json({
+      status: 400,
+      message: "The password must be between 8 and 255 characters.",
+    });
+  }
+
+  try {
+    const user = await prisma.user.create({ data: { name, password } });
+    const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+    return res
+      .status(201)
+      .cookie(process.env.COOKIE_NAME, token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        path: "/",
+        secure: false,
+        httpOnly: true,
+        sameSite: "strict",
+      })
+      .json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      status: 500,
+      message:
+        "There was an internal error. Please try again or try using a different nickname.",
+    });
+  }
 }
 
 module.exports = {
