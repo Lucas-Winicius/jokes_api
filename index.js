52f6c0f635db2c748da8f38a64305c50b4ef9e2a
Lucas-Winicius <lucasclucas7@gmail.com>
2023-06-01 11:12:57 -0300

    cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-require("dotenv").config();
-const express = require("express");
 const cookieParser = require("cookie-parser");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
 const routes = require("./src/router");
 
 const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SIGNATURE));
